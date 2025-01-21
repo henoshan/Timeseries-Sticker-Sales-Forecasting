030b9c84ab2b2c8f4dd8b0b83e14e46d607100fc
Henoshan Athimoolanathan <107171366+henoshan@users.noreply.github.com>
2025-01-21 16:17:46 +0530

    Update Main.py
    Update in loadCSV function

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
 pip install pandas
 
-def loadCSV():
+def loadCSV(filePath):
     import pandas as pd
     from prophet import Prophet
     import matplotlib.pyplot as plt
 
     # Load your training data
-    train = pd.read_csv('/kaggle/input/playground-series-s5e1/train.csv')
+    train = pd.read_csv(filePath)
 
     # Check the first few rows
     print(train.head())
 
     return train
+
+train_dataset = loadCSV('/kaggle/input/playground-series-s5e1/train.csv')
